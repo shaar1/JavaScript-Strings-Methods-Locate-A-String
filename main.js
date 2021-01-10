398efd41a7ca66935c93a4708ea1af5193be6930
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 18:36:00 +0100

    ett exampel om indexOf och hur ser ut det i String

---
+++ main.js
@@ -0,0 +1,32 @@
+/* gobal console, alert. prompt*/
+ // String 
+ // IndexOf
+
+ /*
+Syntax: indexOf (Value, Start Position ) // Default = 0
+ */
+
+/*
+var myString = "Im In Love Whith JavaScript",
+
+    myWord = myString.indexOf("Whith");
+
+console.log(myWord);
+*/
+
+/*
+var myString = "Im In Love Whith JavaScript",
+
+    myWord = myString.indexOf("Love");
+
+console.log(myWord);
+*/
+
+var myString = "Im In Love Whith JavaScript",
+
+    myWord = myString.indexOf("Love", 0);
+
+console.log(myWord);
+
+
+
