68eef60f22f4c7d7443ca2516d4f76c7e3301212
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 18:40:36 +0100

    ett exampel om lastindexof

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
  /*
 Syntax: indexOf (Value, Start Position ) // Default = 0
+Syntax: lastIndexOf (Value, Start Position ) // Default = 0
  */
 
 /*
@@ -22,11 +23,20 @@ var myString = "Im In Love Whith JavaScript",
 console.log(myWord);
 */
 
+/*
 var myString = "Im In Love Whith JavaScript And In Love With PHP",
 
     myWord = myString.indexOf("Love", 1);
 
 console.log(myWord);
+*/
+
+var myString = "Im In Love Whith JavaScript And In Love With PHP",
+
+    myWord = myString.lastIndexOf("Love");
+
+console.log(myWord);
+
 
 
 
